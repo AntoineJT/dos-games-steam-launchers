80698e17cd9b0781196c4a280ca53053afaa2b4e
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-05-29 13:31:38 +0200

    Remove ignored parameter

--- heretic.c
+++ heretic.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
 
 int main(void) {
-    system(".\\chocolate-heretic\\chocolate-heretic.exe -iwad .\\base\\HERETIC.WAD -fullscreen -exit");
+    system(".\\chocolate-heretic\\chocolate-heretic.exe -iwad .\\base\\HERETIC.WAD -fullscreen");
 }
--- hexen.c
+++ hexen.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
 
 int main(void) {
-    system(".\\chocolate-hexen\\chocolate-hexen.exe -iwad .\\base\\HEXEN.WAD -fullscreen -exit");
+    system(".\\chocolate-hexen\\chocolate-hexen.exe -iwad .\\base\\HEXEN.WAD -fullscreen");
 }

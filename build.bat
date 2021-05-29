ddb514ca016f30f392912bc8bb3cda6468fe2807
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-05-29 13:43:08 +0200

    Add HeXen: Deathkings of the Dark Citadel

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 gcc blood.c -O3 -o dosbox_bloodgdx.exe
 gcc heretic.c -O3 -o dosbox_chocolate-heretic.exe
 gcc hexen.c -O3 -o dosbox_chocolate-hexen.exe
+gcc hexendd.c -O3 -o dosbox_chocolate-hexendd.exe
---
+++ hexendd.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+
+int main(void) {
+    system(".\\chocolate-hexen\\chocolate-hexen.exe -iwad .\\base\\HEXEN.WAD -file .\\base\\HEXDD.WAD -fullscreen");
+}

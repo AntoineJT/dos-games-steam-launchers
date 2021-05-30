af9bfee735e3c5575af18666e8b7a3c76e4bec3c
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-05-30 17:06:41 +0200

    Better version of heretic.c

--- heretic.c
+++ heretic.c
@@ -8,21 +8,44 @@ enum cmd_color {
     WHITE = 15
 };
 
+const char* folders[] = {
+    "chocolate-heretic"
+};
+
+const char* commands[] = {
+    "chocolate-heretic.exe -iwad ..\\base\\HERETIC.WAD"
+};
+
+#ifdef HERETIC
+    #define GAME 0
+#elif defined(HEXEN)
+    #define GAME 1
+    #error unsupported for now
+#elif defined(HEXENDD)
+    #define GAME 2
+    #error unsupported for now
+#endif
+
+#define CHOCO_FOLDER folders[GAME]
+#define CHOCO_CMD commands[GAME]
+
 int main(void)
 {
-    if (chdir("chocolate-heretic") != 0) {
+    if (chdir(CHOCO_FOLDER) != 0) {
         const HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+        const char* folder = CHOCO_FOLDER;
 
         SetConsoleTextAttribute(console, RED);
-        puts("chocolate-heretic folder not found!");
+        printf("%s folder not found!\n", folder);
+
         SetConsoleTextAttribute(console, WHITE);
-        puts("Make sure you have copied chocolate-heretic files "
-            "in the root directory/chocolate-heretic of the game "
+        printf("Make sure you have copied %s files "
+            "in the root directory/%s of the game "
             "\n" "AND that you're running it from Steam."
             "\n" "You can't run this file outside of Steam directly!"
-            "\n");
+            "\n", folder, folder);
         system("pause");
         return EXIT_FAILURE;
     }
-    system("chocolate-heretic.exe -iwad ..\\base\\HERETIC.WAD");
+    system(CHOCO_CMD);
 }

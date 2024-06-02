23f47a1d839b8f369997c92fa9bb023002a3180a
Antoine James Tournepiche <26577763+AntoineJT@users.noreply.github.com>
2024-06-02 22:49:22 +0200

    Merge blood and red faction launchers code in wrapper.c (#10)

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-gcc blood.c -O3 -o dosbox_bloodgdx.exe
 gcc choco.c -O3 -DDOOM -o dosbox_chocolate-doom.exe
 gcc choco.c -O3 -DHERETIC -o dosbox_chocolate-heretic.exe
 gcc choco.c -O3 -DHEXEN -o dosbox_chocolate-hexen.exe
 gcc choco.c -O3 -DHEXENDD -o dosbox_chocolate-hexendd.exe
-gcc rf.c -O3 -o RedFaction.exe
+gcc wrapper.c -O3 -DBLOOD -o dosbox_bloodgdx.exe
+gcc wrapper.c -O3 -DRED_FACTION -o RedFaction.exe

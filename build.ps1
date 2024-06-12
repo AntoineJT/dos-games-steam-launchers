c78a702e9c59d2fae30ec342ae5d9138fcbfa0b9
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2024-06-13 00:42:06 +0200

    spaces

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 Copy-Item -Path dist_template -Destination dist -Recurse
 
 gcc wrapper.c -O3 -DBLOOD -o "dist/Blood/BuildGDX/dosbox.exe"
-gcc choco.c -O3 -DDOOM -o "dist/Doom/ChocolateDoom/dosbox.exe"
-gcc choco.c -O3 -DHERETIC -o "dist/Heretic/ChocolateHeretic/dosbox.exe"
-gcc choco.c -O3 -DHEXEN -o "dist/Hexen/ChocolateHexen/dosbox.exe"
-gcc choco.c -O3 -DHEXENDD -o "dist/Hexen Deathkings of the Dark Citadel/ChocolateHexen/dosbox.exe"
-gcc wrapper.c -O3 -DRED_FACTION -o "dist/Red Faction/DashFaction/RedFaction.exe"
+gcc choco.c -O3 -DDOOM -o "dist/Doom/Chocolate Doom/dosbox.exe"
+gcc choco.c -O3 -DHERETIC -o "dist/Heretic/Chocolate Heretic/dosbox.exe"
+gcc choco.c -O3 -DHEXEN -o "dist/Hexen/Chocolate Hexen/dosbox.exe"
+gcc choco.c -O3 -DHEXENDD -o "dist/Hexen Deathkings of the Dark Citadel/Chocolate Hexen/dosbox.exe"
+gcc wrapper.c -O3 -DRED_FACTION -o "dist/Red Faction/Dash Faction/RedFaction.exe"
